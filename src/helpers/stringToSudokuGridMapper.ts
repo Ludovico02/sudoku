45648c48d43d360b5ad00f45436862d39e73659e
Ludovico Gardenal <ludovico02@outlook.com>
2026-07-25 18:03:51 +0200

    feat: added helper to map api response to a grid

---
+++ src/helpers/stringToSudokuGridMapper.ts
@@ -0,0 +1,33 @@
+import { CellState, SudokuGrid } from "@/types/sudoku";
+
+export default function stringToSudokuGridMapper(puzzle: string, solution: string): SudokuGrid {
+    const grid: SudokuGrid = [];
+
+    if (puzzle.length !== 81 || solution.length !== 81) {
+        throw new Error("Puzzle or Solutions are not 81 characters long");
+    }
+
+    for (let row = 0; row < 9; row++) {
+        const currentRow: CellState[] = [];
+
+        for (let col = 0; col < 9; col++) {
+            const stringIndex = row * 9 + col;
+
+            const value = parseInt(puzzle[stringIndex]);
+            const solutionValue = parseInt(solution[stringIndex]);
+
+            currentRow.push({
+                row,
+                col,
+                value,
+                solutionValue,
+                isFixed: value !== 0,
+                isError: false
+            });
+        }
+
+        grid.push(currentRow);
+    }
+
+    return grid;
+}
