f902869085f60fae15a54de134937c92cd4323cf
Ludovico Gardenal <ludovico02@outlook.com>
2026-07-27 14:35:00 +0200

    feat: added selected cell and input logic in useSudoku

--- src/hooks/useSudoku.ts
+++ src/hooks/useSudoku.ts
@@ -10,6 +10,7 @@ export function useSudoku() {
     difficulty: "easy",
     status: "idle",
     mistakes: 0,
+    selectedCell: null,
   });
 
   const requestIdRef = useRef(0);
@@ -17,7 +18,12 @@ export function useSudoku() {
   const startNewGame = useCallback(async (difficulty: Difficulty) => {
     const currentRequestId = ++requestIdRef.current;
 
-    setGameState((prev) => ({ ...prev, status: "loading", difficulty }));
+    setGameState((prev) => ({
+      ...prev,
+      status: "loading",
+      difficulty,
+      selectedCell: null,
+    }));
 
     try {
       const data = await fetchSudoku(difficulty);
@@ -33,6 +39,7 @@ export function useSudoku() {
         difficulty,
         status: "playing",
         mistakes: 0,
+        selectedCell: null,
       });
     } catch (error) {
       setGameState((prev) => ({ ...prev, status: "idle" }));
@@ -42,8 +49,54 @@ export function useSudoku() {
     }
   }, []);
 
+  const selectCell = useCallback((row: number, col: number) => {
+    setGameState((prev) => {
+      if (prev.status !== "playing") return prev;
+      if (prev.grid[row][col].isFixed) return prev;
+
+      return { ...prev, selectedCell: { row, col } };
+    });
+  }, []);
+
+  const inputNumber = useCallback((number: number) => {
+    setGameState((prev) => {
+      if (prev.status !== "playing" || !prev.selectedCell) return prev;
+
+      const { row, col } = prev.selectedCell;
+      const targetCell = prev.grid[row][col];
+
+      // If it's correct already refuse the input
+      if (targetCell.value === targetCell.solutionValue) return prev;
+
+      // Check if input is correct
+      const isCorrect = number === targetCell.solutionValue;
+
+      const newGrid = prev.grid.map((r) => [...r]);
+
+      newGrid[row][col] = {
+        ...targetCell,
+        value: number,
+        isError: !isCorrect, // If input is incorrect show the error
+      };
+
+      const newMistakes = isCorrect ? prev.mistakes : prev.mistakes + 1;
+      const newStatus = newMistakes >= 3 ? "game-over" : "playing";
+
+      // TODO Check if user has won
+
+      return {
+        ...prev,
+        grid: newGrid,
+        mistakes: newMistakes,
+        status: newStatus,
+      };
+    });
+  }, []);
+
   return {
     gameState,
     startNewGame,
+    selectCell,
+    inputNumber
   };
 }
